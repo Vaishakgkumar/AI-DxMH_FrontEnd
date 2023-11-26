fd3e16c555686967ec31024fff8b46a44d1d93fa
vaishakgkumar <vaishakgkumar@gmail.com>
2023-11-26 23:49:19 +0530

    Website frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,11 +48,11 @@ export default function RootLayout({
 							<Link
 								isExternal
 								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+								href=""
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<span className="text-default-600">Made with 🩺 by</span>
+								<p className="text-primary">Team Doc</p>
 							</Link>
 						</footer>
 					</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,58 @@
+"use client";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
+import { title, subtitle, title2, title3 } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import { Card, CardHeader, CardBody, CardFooter, Image, Button, Slider } from "@nextui-org/react";
+import { use, useState } from "react";
+import { Textarea } from "@nextui-org/react";
 
 export default function Home() {
+	const [inputText, setInputText] = useState("");
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInputText(event.target.value);
+	};
+
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+			<div className="inline-block max-w-1lg text-center justify-center w-full">
+				<h1 className={title2({ size: "lg" })}>Your Personal&nbsp;</h1>
+				<h1 className={title3({ color: "yellow", size: "lg" })}>Medical Bot &nbsp;</h1>
 				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
+				<h2 className={subtitle({ size: "sm" })}>Power of Medical Diagnosis&nbsp;</h2>
+				<h2 className={subtitle({ size: "sm", color: "blue" })}>in a very fast and modern way.&nbsp;</h2>
 			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
+			<Card
+				isBlurred
+				radius="lg"
+				className="Border-none bg-background/60 dark:bg-default-100/50 max-w-[610px] w-full"
+				shadow="lg"
+			>
+				<CardHeader></CardHeader>
+				<CardBody>
+					<div className="flex justify-center">
+						<Image src="/images/aidm.png" width={600} height={200} alt="aidm" className="rounded-full" />
+					</div>
+				</CardBody>
+				<CardFooter>
+					<div className="max-w-[610px] w-full">
+						<Textarea
+							key="bordered"
+							variant="bordered"
+							label="Enter your Symptoms"
+							labelPlacement="outside"
+							radius="full"
+							maxRows={2.4}
+							value={inputText}
+							onChange={handleInputChange}
+						/>
+					</div>
+				</CardFooter>
+			</Card>
 		</section>
 	);
 }
